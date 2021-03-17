3509fd07aba68de7464813decd2716bc38f04267
Firas Khalil Khana <firasuke@gmail.com>
2021-03-17 14:48:40 +0200

    Improve argument parser

---
+++ src/argparse.rs
@@ -0,0 +1,100 @@
+use std::env;
+use std::process;
+
+use super::help;
+
+pub fn radula_argparse(radula_genome: &str) {
+    let mut args = env::args().skip(1);
+
+    if args.len() < 1 {
+        help::radula_open(help::RADULA_HELP);
+        process::exit(1);
+    }
+
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "b" | "-b" | "--behave" => match args.next().as_deref() {
+                Some("b") | Some("bootstrap") => match args.next().as_deref() {
+                    Some("c") | Some("clean") => println!("Do nothing"),
+                    Some("d") | Some("distclean") => println!("Do nothing"),
+
+                    Some("h") | Some("-h") | Some("--help") => {
+                        help::radula_open(help::RADULA_BEHAVE_BOOTSTRAP_HELP)
+                    }
+
+                    Some("i") | Some("image") => println!("Do nothing"),
+                    Some("l") | Some("list") => println!("Do nothing"),
+                    Some("r") | Some("require") => println!("Do nothing"),
+                    Some("s") | Some("release") => println!("Do nothing"),
+                    Some("t") | Some("toolchain") => println!("Do nothing"),
+                    Some("x") | Some("cross") => println!("Do nothing"),
+                    _ => {
+                        help::radula_open(help::RADULA_BEHAVE_BOOTSTRAP_HELP);
+                        process::exit(1);
+                    }
+                },
+                Some("e") | Some("envenomate") => match args.next().as_deref() {
+                    Some("h") | Some("-h") | Some("--help") => {
+                        help::radula_open(help::RADULA_BEHAVE_ENVENOMATE_HELP);
+                    }
+
+                    _ => {
+                        help::radula_open(help::RADULA_BEHAVE_ENVENOMATE_HELP);
+                        process::exit(1);
+                    }
+                },
+                Some("i") | Some("binary") => match args.next().as_deref() {
+                    Some("h") | Some("-h") | Some("--help") => {
+                        help::radula_open(help::RADULA_BEHAVE_BINARY_HELP);
+                    }
+
+                    _ => {
+                        help::radula_open(help::RADULA_BEHAVE_BINARY_HELP);
+                        process::exit(1);
+                    }
+                },
+
+                Some("h") | Some("-h") | Some("--help") => {
+                    help::radula_open(help::RADULA_BEHAVE_HELP)
+                }
+
+                _ => {
+                    help::radula_open(help::RADULA_BEHAVE_HELP);
+                    process::exit(1);
+                }
+            },
+            "c" | "-c" | "--ceras" => match args.next().as_deref() {
+                Some("n") | Some("nom") | Some("name") => println!("Do nothing"),
+
+                Some("h") | Some("-h") | Some("--help") => {
+                    help::radula_open(help::RADULA_CERAS_HELP)
+                }
+
+                Some("v") | Some("ver") | Some("version") => println!("Do nothing"),
+                Some("u") | Some("url") | Some("source") => println!("Do nothing"),
+                Some("s") | Some("sum") | Some("checksum") | Some("sha512sum") => {
+                    println!("Do nothing")
+                }
+                Some("y") | Some("cys") | Some("cyst") | Some("cysts") => println!("Do nothing"),
+                Some("c") | Some("cnt") | Some("concentrate") | Some("concentrates") => {
+                    println!("Do nothing")
+                }
+                Some("l") | Some("lic") | Some("license") | Some("licenses") => {
+                    println!("Do nothing")
+                }
+                _ => {
+                    help::radula_open(help::RADULA_CERAS_HELP);
+                    process::exit(1);
+                }
+            },
+            "g" | "-g" | "--genome" => println!("{}", radula_genome),
+            "h" | "-h" | "--help" => help::radula_open(help::RADULA_HELP),
+
+            "v" | "-v" | "--version" => println!("{}", help::RADULA_VERSION),
+            _ => {
+                help::radula_open(help::RADULA_HELP);
+                process::exit(1);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,11 @@
-use std::io::{self, Write};
-use std::process;
-use std::{
-    env::{self, current_dir},
-    path::Path,
-    path::PathBuf,
-};
+use std::env;
 
+mod argparse;
 mod help;
 mod paths;
 mod variables;
 
 fn main() {
-    let mut args = env::args();
-
-    while let Some(arg) = args.next() {
-        match arg.as_str() {
-            "b" => match args.next().unwrap().as_str() {
-                "c" => {
-                    println!("Do something");
-                }
-                _ => {
-                    println!("Do nothing");
-                }
-            },
-            _ => println!("Do nothing"),
-        }
-    }
-
     // Disable unicode (supposedly speeds up running shell scripts)
     env::set_var("LANG", "C");
     env::set_var("LC_ALL", "C");
@@ -36,88 +15,29 @@ fn main() {
     let radula_ccache = true;
     // Prevents running the basic check function as it's not ready yet...
     let radula_check = false;
+
+    #[cfg(target_arch = "aarch64")]
+    let radula_genome = "aarch64";
+
+    #[cfg(target_arch = "arm")]
+    let radula_genome = "armv6zk";
+
+    #[cfg(target_arch = "x86")]
+    let radula_genome = "x86";
+
+    #[cfg(target_arch = "x86_64")]
     let radula_genome = "x86-64";
+
     let radula_parallel = true;
 
-    // Required when working with the following cerata `radula`, `hydroskeleton`, `pcre2`
-    env::set_var("genome", "x86-64");
+    // Required when working with the following cerata `radula`, `hydroskeleton`
+    env::set_var("genome", radula_genome);
 
-    variables::radula_behave_ccache_variables();
-
-    let radula_glaucus_directory = current_dir().unwrap();
-
-    if args.len() < 2 {
-        help::radula_open(help::RADULA_HELP);
-        process::exit(1);
+    if radula_ccache {
+        variables::radula_behave_ccache_variables();
     }
 
-    //match args[1].as_str() {
-    //"b" | "-b" | "--behave" => match args[2].as_str() {
-    //"b" | "bootstrap" => match args[3].as_str() {
-    //"c" | "clean" => println!("Do nothing"),
-    //"d" | "distclean" => println!("Do nothing"),
+    let radula_glaucus_directory = env::current_dir().unwrap();
 
-    //"h" | "-h" | "--help" => help::radula_open(help::RADULA_BEHAVE_BOOTSTRAP_HELP),
-
-    //"i" | "image" => println!("Do nothing"),
-    //"l" | "list" => println!("Do nothing"),
-    //"r" | "require" => println!("Do nothing"),
-    //"s" | "release" => println!("Do nothing"),
-    //"t" | "toolchain" => println!("Do nothing"),
-    //"x" | "cross" => println!("Do nothing"),
-    //_ => {
-    //help::radula_open(help::RADULA_BEHAVE_BOOTSTRAP_HELP);
-    //process::exit(1);
-    //}
-    //},
-    //"e" | "envenomate" => match args[3].as_str() {
-    //"h" | "-h" | "--help" => help::radula_open(help::RADULA_BEHAVE_ENVENOMATE_HELP),
-
-    //_ => {
-    //help::radula_open(help::RADULA_BEHAVE_ENVENOMATE_HELP);
-    //process::exit(1);
-    //}
-    //},
-
-    //"h" | "-h" | "--help" => help::radula_open(help::RADULA_BEHAVE_HELP),
-
-    //"i" | "binary" => match args[3].as_str() {
-    //"h" | "-h" | "--help" => help::radula_open(help::RADULA_BEHAVE_BINARY_HELP),
-
-    //_ => {
-    //help::radula_open(help::RADULA_BEHAVE_BINARY_HELP);
-    //process::exit(1);
-    //}
-    //},
-    //_ => {
-    //help::radula_open(help::RADULA_BEHAVE_HELP);
-    //process::exit(1);
-    //}
-    //},
-    //"c" | "-c" | "--ceras" => match args[2].as_str() {
-    //"n" | "nom" | "name" => println!("Do nothing"),
-
-    //"h" | "-h" | "--help" => help::radula_open(help::RADULA_CERAS_HELP),
-
-    //"v" | "ver" | "version" => println!("Do nothing"),
-    //"u" | "url" | "source" => println!("Do nothing"),
-    //"s" | "sum" | "checksum" | "sha512sum" => println!("Do nothing"),
-    //"y" | "cys" | "cyst" | "cysts" => println!("Do nothing"),
-    //"c" | "cnt" | "concentrate" | "concentrates" => println!("Do nothing"),
-    //"l" | "lic" | "license" | "licenses" => println!("Do nothing"),
-    //_ => {
-    //help::radula_open(help::RADULA_CERAS_HELP);
-    //process::exit(1);
-    //}
-    //},
-    //"g" | "-g" | "--genome" => println!("{}", radula_genome),
-
-    //"h" | "-h" | "--help" => help::radula_open(help::RADULA_HELP),
-
-    //"v" | "-v" | "--version" => println!("{}", help::RADULA_VERSION),
-    //_ => {
-    //help::radula_open(help::RADULA_BEHAVE_HELP);
-    //process::exit(1);
-    //}
-    //}
+    argparse::radula_argparse(radula_genome);
 }
