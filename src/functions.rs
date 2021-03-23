d8f86ffbfb25282d932723a3e49237235e07a74e
Firas Khalil Khana <firasuke@gmail.com>
2021-03-24 00:03:30 +0200

    Revamp swallow and add fetch and verify to it

--- src/functions.rs
+++ src/functions.rs
@@ -6,64 +6,7 @@ use std::string::String;
 
 use super::paths;
 
-pub fn radula_behave_fetch(x: &'static str) {
-    // Receive the variables from the `ceras` file
-    let y: [String; 8] = radula_behave_source(x);
-
-    // Get the path of the source directory
-    let z = String::from(
-        Path::new(&env::var("SRCD").unwrap())
-            .join(&y[0])
-            .to_str()
-            .unwrap(),
-    );
-
-    if y[1] == paths::RADULA_GIT {
-        // Clone the `git` repo
-        Command::new(paths::RADULA_GIT)
-            .args(&["clone", &y[3], &z])
-            .spawn();
-        // Checkout the freshly cloned `git` repo at the specified commit number
-        Command::new(paths::RADULA_GIT)
-            .args(&["-C", &z, "checkout", &y[2]])
-            .spawn();
-    } else {
-        let w = String::from(
-            Path::new(&z)
-                .join(Path::new(&y[3]).file_name().unwrap())
-                .to_str()
-                .unwrap(),
-        );
-
-        Command::new(paths::RADULA_MKDIR)
-            .args(&[paths::RADULA_MKDIR_FLAGS, &z])
-            .stdout(Stdio::null())
-            .spawn();
-
-        Command::new(paths::RADULA_CURL)
-            .args(&[paths::RADULA_CURL_FLAGS, &w, &y[3]])
-            .spawn();
-
-        // verify
-        write!(
-            Command::new(paths::RADULA_CHECKSUM)
-                .arg(paths::RADULA_CHECKSUM_FLAGS)
-                .stdin(Stdio::piped())
-                .spawn()
-                .unwrap()
-                .stdin
-                .unwrap(),
-            "{}",
-            [&y[4], " ", &w].concat()
-        )
-        .unwrap();
-
-        Command::new(paths::RADULA_TAR)
-            .args(&["xvf", &w, "-C", &z])
-            .stdout(Stdio::null())
-            .spawn();
-    }
-}
+pub fn radula_behave_construct() {}
 
 // Sources the `ceras` file and returns an array of strings representing the
 // variables inside of it
@@ -101,4 +44,71 @@ pub fn radula_behave_source(x: &'static str) -> [String; 8] {
     return y;
 }
 
-pub fn radula_behave_swallow() {}
+// Fetches and verifies ceras's source
+pub fn radula_behave_swallow(x: &'static str) {
+    // Receive the variables from the `ceras` file
+    let y: [String; 8] = radula_behave_source(x);
+
+    // Get the path of the source directory
+    let z = String::from(
+        Path::new(&env::var("SRCD").unwrap())
+            .join(&y[0])
+            .to_str()
+            .unwrap(),
+    );
+
+    let w = String::from(
+        Path::new(&z)
+            .join(Path::new(&y[3]).file_name().unwrap())
+            .to_str()
+            .unwrap(),
+    );
+
+    // verify
+    let radula_behave_verify = || {
+        write!(
+            Command::new(paths::RADULA_CHECKSUM)
+                .arg(paths::RADULA_CHECKSUM_FLAGS)
+                .stdin(Stdio::piped())
+                .spawn()
+                .unwrap()
+                .stdin
+                .unwrap(),
+            "{}",
+            [&y[4], " ", &w].concat()
+        )
+        .unwrap();
+    };
+
+    if !Path::is_dir(Path::new(&z)) {
+        if y[1] == paths::RADULA_GIT {
+            // Clone the `git` repo
+            Command::new(paths::RADULA_GIT)
+                .args(&["clone", &y[3], &z])
+                .spawn();
+            // Checkout the freshly cloned `git` repo at the specified commit number
+            Command::new(paths::RADULA_GIT)
+                .args(&["-C", &z, "checkout", &y[2]])
+                .spawn();
+        } else {
+            Command::new(paths::RADULA_MKDIR)
+                .args(&[paths::RADULA_MKDIR_FLAGS, &z])
+                .stdout(Stdio::null())
+                .spawn();
+
+            Command::new(paths::RADULA_CURL)
+                .args(&[paths::RADULA_CURL_FLAGS, &w, &y[3]])
+                .spawn();
+
+            // verify
+            radula_behave_verify();
+
+            Command::new(paths::RADULA_TAR)
+                .args(&["xvf", &w, "-C", &z])
+                .stdout(Stdio::null())
+                .spawn();
+        }
+    } else {
+        radula_behave_verify();
+    }
+}
