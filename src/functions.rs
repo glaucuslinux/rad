344ed9f1401f3ec2987d05484ef26d5c2b384f99
Firas Khalil Khana <firasuke@gmail.com>
2021-05-17 02:45:16 +0300

    Replace mlocate with plocate

--- src/functions.rs
+++ src/functions.rs
@@ -241,7 +241,7 @@ fn radula_behave_bootstrap_cross_construct() {
     radula_behave_construct_cross("findutils");
     radula_behave_construct_cross("grep");
     radula_behave_construct_cross("hostname");
-    radula_behave_construct_cross("mlocate");
+    radula_behave_construct_cross("plocate");
     radula_behave_construct_cross("sed");
     radula_behave_construct_cross("which");
 
