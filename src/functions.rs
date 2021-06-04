4bb66c1d22b49c457629a3e0623294b2d2c623c6
Firas Khalil Khana <firasuke@gmail.com>
2021-06-05 02:54:44 +0300

    Implement flags_environment

--- src/functions.rs
+++ src/functions.rs
@@ -746,7 +746,35 @@ fn radula_behave_construct(x: &'static str, y: &'static str) {
 }
 
 // This will be used whether we're bootstrapping or not so don't add _bootstrap_ to its name
-fn radula_behave_flags_environment() {}
+fn radula_behave_flags_environment() {
+    env::set_var(
+        constants::RADULA_ENVIRONMENT_FLAGS_C_COMPILER,
+        [
+            constants::RADULA_FLAGS_C_COMPILER,
+            " ",
+            &env::var(constants::RADULA_ENVIRONMENT_ARCHITECTURE_FLAGS).unwrap(),
+        ]
+        .concat(),
+    );
+    env::set_var(
+        constants::RADULA_ENVIRONMENT_FLAGS_CXX_COMPILER,
+        [
+            &env::var(constants::RADULA_ENVIRONMENT_FLAGS_C_COMPILER).unwrap(),
+            " ",
+            constants::RADULA_FLAGS_CXX_COMPILER,
+        ]
+        .concat(),
+    );
+    env::set_var(
+        constants::RADULA_ENVIRONMENT_FLAGS_LINKER,
+        [
+            constants::RADULA_FLAGS_LINKER,
+            " ",
+            &env::var(constants::RADULA_ENVIRONMENT_FLAGS_CXX_COMPILER).unwrap(),
+        ]
+        .concat(),
+    );
+}
 
 fn radula_behave_pkg_config_environment() {
     env::set_var(
