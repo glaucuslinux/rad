3d2d9a5e8f3f725b18cf55b5f843f958a3d2b967
Firas Khalil Khana <firasuke@gmail.com>
2022-01-15 00:23:05 +0200

    Improve error handling

--- src/clean.rs
+++ src/clean.rs
@@ -9,42 +9,42 @@ use super::constants;
 use tokio::fs;
 
 pub async fn radula_behave_bootstrap_clean() -> Result<(), Box<dyn std::error::Error>> {
-    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_CROSS).unwrap()).await?;
+    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_CROSS)?).await?;
 
-    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_LOGS).unwrap()).await?;
+    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_LOGS)?).await?;
 
     fs::remove_dir_all(
-        &env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_TOOLCHAIN_TEMPORARY_BUILDS).unwrap(),
+        &env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_TOOLCHAIN_TEMPORARY_BUILDS)?,
     )
     .await?;
 
     fs::remove_dir_all(
-        &env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_CROSS_TEMPORARY_BUILDS).unwrap(),
+        &env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_CROSS_TEMPORARY_BUILDS)?,
     )
     .await?;
 
-    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_TOOLCHAIN).unwrap())
+    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_TOOLCHAIN)?)
         .await?;
 
     Ok(())
 }
 
 pub async fn radula_behave_bootstrap_distclean() -> Result<(), Box<dyn std::error::Error>> {
-    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_BACKUPS).unwrap()).await?;
+    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_BACKUPS)?).await?;
 
     fs::remove_file(
-        Path::new(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_GLAUCUS).unwrap())
+        Path::new(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_GLAUCUS)?)
             .join(constants::RADULA_FILE_GLAUCUS_IMAGE),
     ).await?;
 
-    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_SOURCES).unwrap()).await?;
+    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_SOURCES)?).await?;
 
     radula_behave_bootstrap_clean().await?;
 
     // Only remove `RADULA_ENVIRONMENT_DIRECTORY_TEMPORARY` completely after
     // `RADULA_ENVIRONMENT_DIRECTORY_TOOLCHAIN_BUILDS` and
     // `RADULA_ENVIRONMENT_DIRECTORY_CROSS_BUILDS` are removed
-    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_TEMPORARY).unwrap()).await?;
+    fs::remove_dir_all(&env::var(constants::RADULA_ENVIRONMENT_DIRECTORY_TEMPORARY)?).await?;
 
     Ok(())
 }
--- src/flags.rs
+++ src/flags.rs
@@ -6,20 +6,20 @@ use std::env;
 use super::constants;
 
 // This will be used whether we're bootstrapping or not so don't add _bootstrap_ to its name
-pub fn radula_behave_flags_environment() {
+pub fn radula_behave_flags_environment() -> Result<(), Box<dyn std::error::Error>> {
     env::set_var(
         constants::RADULA_ENVIRONMENT_FLAGS_C_COMPILER,
         [
             constants::RADULA_FLAGS_C_COMPILER,
             " ",
-            &env::var(constants::RADULA_ENVIRONMENT_ARCHITECTURE_FLAGS).unwrap(),
+            &env::var(constants::RADULA_ENVIRONMENT_ARCHITECTURE_FLAGS)?,
         ]
         .concat(),
     );
     env::set_var(
         constants::RADULA_ENVIRONMENT_FLAGS_CXX_COMPILER,
         [
-            &env::var(constants::RADULA_ENVIRONMENT_FLAGS_C_COMPILER).unwrap(),
+            &env::var(constants::RADULA_ENVIRONMENT_FLAGS_C_COMPILER)?,
             " ",
             constants::RADULA_FLAGS_CXX_COMPILER,
         ]
@@ -30,8 +30,10 @@ pub fn radula_behave_flags_environment() {
         [
             constants::RADULA_FLAGS_LINKER,
             " ",
-            &env::var(constants::RADULA_ENVIRONMENT_FLAGS_C_COMPILER).unwrap(),
+            &env::var(constants::RADULA_ENVIRONMENT_FLAGS_C_COMPILER)?,
         ]
         .concat(),
     );
+
+    Ok(())
 }
