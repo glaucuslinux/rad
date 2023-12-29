117cb62c067cbc2ec7bd9dffbcf8889557ed72d6
Firas Khalil Khana <firasuke@gmail.com>
2023-12-29 20:38:43 +0300

    Simplify radula

--- src/ceras.nim
+++ src/ceras.nim
@@ -29,19 +29,19 @@ proc radula_ceras_extract_source*(file: string): bool =
   return false
 
 # Return the full path to the `ceras` file
-func radula_ceras_path_ceras*(nom: string): string =
+func radula_ceras_path*(nom: string): string =
   RADULA_PATH_RADULA_CLUSTERS_GLAUCUS / nom / RADULA_FILE_CERAS
 
 # Check if the full path to the `ceras` file exists
-proc radula_ceras_exist_ceras*(nom: string) =
-  if not fileExists(radula_ceras_path_ceras(nom)):
+proc radula_ceras_exist*(nom: string) =
+  if not fileExists(radula_ceras_path(nom)):
     styledEcho fgRed, styleBright, &"{\"Abort\":13} :! {nom:48}{\"nom\":13}{now().format(\"hh:mm:ss tt\")}", resetStyle
 
     radula_exit(QuitFailure)
 
 # Parse the `ceras` file
-proc radula_ceras_parse_ceras*(nom: string): TomlValueRef =
-  parseFile(radula_ceras_path_ceras(nom))
+proc radula_ceras_parse*(nom: string): TomlValueRef =
+  parseFile(radula_ceras_path(nom))
 
 # Return the full path to the `ceras` source directory
 func radula_ceras_path_source*(nom: string): string =
@@ -49,9 +49,9 @@ func radula_ceras_path_source*(nom: string): string =
 
 proc radula_ceras_print*(cerata: seq[string]) =
   for nom in cerata.deduplicate():
-    radula_ceras_exist_ceras(nom)
+    radula_ceras_exist(nom)
 
-    let ceras = radula_ceras_parse_ceras(nom)
+    let ceras = radula_ceras_parse(nom)
 
     const NONE = ansiForegroundColorCode(fgRed) & "None" & ansiForegroundColorCode(fgDefault)
 
@@ -72,7 +72,7 @@ proc radula_ceras_print_header*() =
 # Resolve concentrates using topological sorting
 proc radula_ceras_resolve_concentrates*(nom: string, concentrates: var Table[string, seq[string]]) =
   # Don't use `{}` because we don't want an empty string "" in our Table
-  concentrates[nom] = try: radula_ceras_parse_ceras(nom)["cnt"].getStr().split() except CatchableError: @[]
+  concentrates[nom] = try: radula_ceras_parse(nom)["cnt"].getStr().split() except CatchableError: @[]
 
   if concentrates[nom].len() > 0:
     for concentrate in concentrates[nom]:
@@ -99,7 +99,7 @@ proc radula_ceras_swallow*(cerata: seq[string]) =
 
   for nom in cerata:
     let
-      ceras = radula_ceras_parse_ceras(nom)
+      ceras = radula_ceras_parse(nom)
 
       ver = ceras{"ver"}.getStr()
       url = ceras{"url"}.getStr()
@@ -258,7 +258,7 @@ proc radula_ceras_envenomate*(cerata: openArray[string], stage = RADULA_DIRECTOR
     log: string
 
   for nom in cerata:
-    radula_ceras_exist_ceras(nom)
+    radula_ceras_exist(nom)
 
     radula_ceras_resolve_concentrates(nom, concentrates)
 
@@ -281,7 +281,7 @@ proc radula_ceras_envenomate*(cerata: openArray[string], stage = RADULA_DIRECTOR
 
   for nom in (if resolve: cluster else: cerata):
     let
-      ceras = radula_ceras_parse_ceras(nom)
+      ceras = radula_ceras_parse(nom)
 
       ver = ceras{"ver"}.getStr()
       cmt = ceras{"cmt"}.getStr()
