b21c6e35ed5610c3b4e64907cc5e22640c482438
Firas Khalil Khana <firasuke@gmail.com>
2024-03-29 23:06:50 +0300

    Remove RADULA_PATCH variable

--- tests/teeth/test_radula_teeth_environment.nim
+++ tests/teeth/test_radula_teeth_environment.nim
@@ -8,16 +8,15 @@ import ../../src/teeth
 
 radula_teeth_environment()
 
-echo "AWK         :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_AWK)
-echo "BISON       :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_BISON)
-echo "FLEX        :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_FLEX)
-echo "LEX         :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_LEX)
-echo "MAKE        :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_MAKE)
-echo "MAKEFLAGS   :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_MAKEFLAGS)
-echo "PATCH       :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_PATCH)
-echo "PKG_CONFIG  :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_PKG_CONFIG)
-echo "RSYNC       :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_RSYNC)
-echo "YACC        :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_YACC)
+echo "AWK           :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_AWK)
+echo "BISON         :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_BISON)
+echo "FLEX          :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_FLEX)
+echo "LEX           :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_LEX)
+echo "MAKE          :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_MAKE)
+echo "MAKEFLAGS     :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_MAKEFLAGS)
+echo "PKG_CONFIG    :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_PKG_CONFIG)
+echo "RADULA_RSYNC  :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_RADULA_RSYNC)
+echo "YACC          :: ", getEnv(RADULA_ENVIRONMENT_TOOTH_YACC)
 
 doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_AWK) == "mawk"
 doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_BISON) == "byacc"
@@ -25,7 +24,6 @@ doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_FLEX) == "flex"
 doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_LEX) == "flex"
 doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_MAKE) == "make"
 doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_MAKEFLAGS) == "-j4 -O"
-doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_PATCH) == "patch --no-backup-if-mismatch --verbose"
 doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_PKG_CONFIG) == "pkgconf"
-doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_RSYNC) == "rsync -vaHAXSx"
+doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_RADULA_RSYNC) == "rsync -vaHAXSx"
 doAssert getEnv(RADULA_ENVIRONMENT_TOOTH_YACC) == "byacc"
