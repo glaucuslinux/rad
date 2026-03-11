9809a669eea24f193ea1aa36a70400dfc4551ba5
Firas Khana <firasuke@gmail.com>
2026-03-11 22:48:23 +0300

    Improve layout

--- src/rad.nim
+++ src/rad.nim
@@ -7,6 +7,106 @@
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 when isMainModule:
-  import options
+  import std/[parseopt, strformat, strutils]
 
-  options()
+  const help = """
+USAGE:
+  rad [OPTIONS] COMMAND [PACKAGES]
+
+OPTIONS:
+  -h, --help         Show help message
+  -p, --no-parallel  Disable parallel build
+  -v, --verbose      Enable verbose build
+  -V, --version      Show rad version
+
+COMMANDS:
+  build          Build packages
+  clean          Clean build cache
+  contents       List package contents
+  info           Show package information
+  list           List built packages
+  search         Search for packages
+  update         Update repositories"""
+
+  const version = """
+rad version 0.1.0
+
+Licensed under the Mozilla Public License Version 2.0 (MPL-2.0)
+Copyright © 2018-2026 Firas Khana"""
+
+  var args: seq[string]
+  var verboseRequested: bool
+  var noParallelRequested: bool
+  var unknownFlags: seq[string]
+
+  for kind, key, val in getopt():
+    case kind
+    of cmdEnd:
+      break
+    of cmdLongOption, cmdShortOption:
+      case key
+      of "h", "help":
+        quit(help, QuitSuccess)
+      of "p", "no-parallel", "noparallel":
+        noParallelRequested = true
+      of "v", "verbose":
+        verboseRequested = true
+      of "V", "version":
+        quit(version, QuitSuccess)
+      else:
+        unknownFlags.add(key)
+    of cmdArgument:
+      args.add(key)
+
+  if unknownFlags.len() > 0:
+    let msg =
+      if unknownFlags.len() == 1:
+        "invalid option: " & unknownFlags[0]
+      else:
+        "invalid options: " & unknownFlags.join(", ")
+    quit(msg, QuitFailure)
+
+  if verboseRequested:
+    echo "do something"
+  if noParallelRequested:
+    echo "do something else"
+
+  if args.len() == 0:
+    quit(help, QuitFailure)
+
+  let cmd = args[0]
+  let cmdArgs = args[1..^1]
+
+  case cmd
+  of "build":
+    # lock()
+    # cleanCache()
+    # buildPackages(remainingArgs(cmdArgs))
+    echo ""
+    echo "build complete"
+  of "clean":
+    # lock()
+    # cleanCache()
+    echo "clean complete"
+  of "contents":
+    # listContents(remainingArgs(cmdArgs))
+    echo "placeholder"
+  of "help":
+    echo help
+  of "info":
+    # showInfo(remainingArgs(cmdArgs))
+    echo "placeholder"
+  of "list":
+    # listPackages()
+    echo "placeholder"
+  of "search":
+    # searchPackages(remainingArgs(cmdArgs))
+    echo "placeholder"
+  of "update":
+    # lock()
+    echo ""
+    echo "update complete"
+  of "version":
+    echo version
+  else:
+    quit(&"invalid command: {cmd}", QuitFailure)
